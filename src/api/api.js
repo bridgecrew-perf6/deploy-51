3e8c03dd7f7b67757b8b9088beb4849458ef3b06
multipassport <mashross@gmail.com>
2022-06-15 17:04:22 +0300

    Change to env variable

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import store from "../store";
 import notify from "../components/notification/instance";
 
 const api = axios.create({
-    baseURL: "http://111.111.111.111:8000/api/",
+    baseURL: process.env.VUE_APP_URL,
 });
 
 // api.headers = { Authorization: `Bearer ${store.state.auth.access}`, 'Content-Type': 'application/json; charset=utf-8' }
